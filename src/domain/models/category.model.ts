d63a149893e9246682217933e8720337e7087560
melis <melissanoemi.laurent@free.fr>
2024-02-10 13:07:35 +0100

    add e2e tests for cards page

--- src/domain/models/category.model.ts
+++ src/domain/models/category.model.ts
@@ -1,10 +1,10 @@
 export enum Category {
-    FIRST,
-    SECOND,
-    THIRD,
-    FOURTH,
-    FIFTH,
-    SIXTH,
-    SEVENTH, //todo est-ce que ça doit être un chiffre ou une string
-    DONE
+    FIRST = "FIRST",
+    SECOND = "SECOND",
+    THIRD = "THIRD",
+    FOURTH =  "FOURTH",
+    FIFTH =  "FIFTH",
+    SIXTH = "SIXTH",
+    SEVENTH = "SEVENTH",
+    DONE = "DONE"
 }
