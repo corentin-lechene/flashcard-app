0120048179f60d7b1b6bfbee08cdaf8a0691c852
AbdallahSlimane <slimane.abdallah75@gmail.com>
2024-02-28 18:31:09 +0100

    fix pr

--- tests/e2e/specs/quizPage.cy.ts
+++ tests/e2e/specs/quizPage.cy.ts
@@ -2,18 +2,18 @@ describe('Quiz Session', () => {
     beforeEach(() => {
         cy.viewport('iphone-8')
         cy.visit('/home');
-        cy.contains('ion-button', 'Commencer').click();
+        cy.get('[data-cy=start-button]').click();
     });
 
     it('loads questions correctly on quiz start', () => {
-        cy.contains('ion-header', 'Questionnaire').should('be.visible');
+        cy.get('[data-cy=quiz-header]').should('be.visible');
         cy.wait(4000)
-        cy.get('ion-textarea').should('be.visible')
-        cy.contains('ion-button', 'Confirmer').should('be.visible');
+        cy.get('[data-cy=answer-input]').should('be.visible')
+        cy.get('[data-cy=confirm-button]').should('be.visible');
     });
 
     it('should allow closing the quiz', () => {
-        cy.get('ion-button').contains('Fermer').click();
+        cy.get('[data-cy=close-button]').click();
     });
 });
 
@@ -21,59 +21,68 @@ describe('Answering a question', () => {
     beforeEach(() => {
         cy.viewport('iphone-8')
         cy.visit('/home');
-        cy.contains('ion-button', 'Commencer').click();
+        cy.get('[data-cy=start-button]').click();
     });
 
     it('allows submitting an answer and shows success message and options', () => {
-        cy.get('ion-textarea').first().type('answer');
-        cy.contains('ion-button', 'Confirmer').click();
+        cy.get('[data-cy=answer-input]').first().type('answer');
+        cy.get('[data-cy=confirm-button]').click();
 
         cy.get('ion-toast').should('exist');
-        cy.contains('ion-button', "C'est la bonne réponse").should('be.visible');
-        cy.contains('ion-button', 'Continuer').should('be.visible');
+        cy.get('[data-cy=correct-answer-button]').should('be.visible');
+        cy.get('[data-cy=continue-button]').should('be.visible');
     });
 });
 
 describe('Navigating questions', () => {
     beforeEach(() => {
-        cy.viewport('iphone-8')
+        cy.viewport('iphone-8');
         cy.visit('/home');
-        cy.contains('ion-button', 'Commencer').click();
+        cy.get('[data-cy=start-button]').click();
     });
 
-    it('goes to the next question after answering', () => {
-        cy.get('ion-textarea').first().type('answer');
-        cy.contains('ion-button', 'Confirmer').click();
+    it('goes to the next question after answering or finishes if no more questions', () => {
+        const answerQuestion = () => {
+            cy.get('body').then(($body) => {
+                if ($body.find('[data-cy=answer-input]').length > 0) {
+                    cy.get('[data-cy=answer-input]').first().type('answer');
+                    cy.get('[data-cy=confirm-button]').click();
+                    cy.get('ion-toast').should('exist').then(($toast) => {
+                        if ($toast.text().includes('Bonne réponse') && $body.find('[data-cy=continue-button]').length > 0) {
+                            cy.get('[data-cy=continue-button]').click();
+                            cy.wait(4000);
+                            answerQuestion();
+                        }
+                    });
+                }
+            });
+        };
 
-        cy.get('ion-toast').should('exist');
-        cy.contains('ion-button', "C'est la bonne réponse").should('be.visible');
-        cy.contains('ion-button', 'Continuer').click();
-        cy.wait(4000)
-        cy.get('ion-textarea').should('be.visible')
-        cy.contains('ion-button', 'Confirmer').should('be.visible');
+        answerQuestion();
     });
 });
+
 
 describe('Finishing the quiz', () => {
     beforeEach(() => {
         cy.viewport('iphone-8');
         cy.visit('/home');
-        cy.contains('ion-button', 'Commencer').click();
+        cy.get('[data-cy=start-button]').click();
     });
 
     it('finishes quiz after last question', () => {
         const answerQuestion = () => {
             cy.get('body').then(($body) => {
-                if ($body.find('ion-button:contains("Continuer")').length > 0) {
-                    cy.get('ion-textarea').first().type('answer');
-                    cy.contains('ion-button', 'Confirmer').click();
+                if ($body.find('[data-cy=continue-button]').length > 0) {
+                    cy.get('[data-cy=answer-input]').first().type('answer');
+                    cy.get('[data-cy=confirm-button]').click();
                     cy.get('ion-toast').should('exist').then(($toast) => {
                         if ($toast.text().includes('Bonne réponse')) {
-                            cy.contains('ion-button', 'Continuer').click();
+                            cy.get('[data-cy=continue-button]').click();
                             cy.wait(4000);
                             answerQuestion();
                         } else if ($toast.text().includes('Mauvaise réponse')) {
-                            cy.contains('ion-button', 'Continuer').click();
+                            cy.get('[data-cy=continue-button]').click();
                             cy.wait(4000);
                             answerQuestion();
                         }
@@ -84,8 +93,3 @@ describe('Finishing the quiz', () => {
         answerQuestion();
     });
 });
-
-
-
-
-
